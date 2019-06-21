e1a70541b1cd5e7b4251ac4cdfc339ecbacc55f9
Yan <850082313@qq.com>
2019-06-21 18:01:16 +0800

    second

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService }  from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+    ) { }
+
+  ngOnInit() {
+  }
+
+}
